ad4d114c2622ad1b753c77ca64921242172331e8
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-10-11 16:06:05 +0300

    Complete!

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import web.model.User;
 import web.service.UserService;
 
 @Controller
+@RequestMapping
 public class UserController {
 
 
@@ -39,13 +39,13 @@ public class UserController {
         return "redirect:/";
     }
 
-    @PostMapping(value = "/delete/{id}")
+    @PostMapping(value = "/delete")
     public String deleteUser(@RequestParam("id") Long id) {
         userService.deleteUserById(id);
         return "redirect:/";
     }
 
-    @GetMapping(value = "/edit/{id}")
+    @GetMapping(value = "/edit")
     public String editPage(@RequestParam("id") Long id, ModelMap model) {
         model.addAttribute("user", userService.getUserById(id));
         return "edit";
