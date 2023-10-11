c27cad37b8632dda0721cf9f668cbf92156f50e9
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-10-11 12:47:43 +0300

    fix

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,19 @@
+package web.dao;
+
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void addUser(User user);
+
+    void deleteUserById(Long id);
+
+    void updateUser(User user);
+
+    List<User> getAllUsers();
+
+    User getUserById(Long id);
+}
